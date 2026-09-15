f147cede41656e5d1d230be7a45e7aeba3109d01
Juan Puello <jpuellov@unicartagena.edu.co>
2026-09-15 15:30:08 -0500

    Conexion a PostgreSQL en Render y credenciales de usuario configuradas

--- src/main/java/com/desarrollo/config/Conexion.java
+++ src/main/java/com/desarrollo/config/Conexion.java
@@ -5,9 +5,10 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class Conexion {
-    private static final String URL = "postgresql://desarrollo_wb_db_user:IvpcqhH4uaApBCBmkep7yqcnrpIyHbJv@dpg-dakq26mk1f9s73cgtk10-a.oregon-postgres.render.com/desarrollo_wb_db";
-    private static final String USER = "postgres";
-    private static final String PASSWORD = "Futurelo#2007";
+    private static final String URL = "jdbc:postgresql://dpg-dakq26mk1f9s73cgtk10-a.oregon-postgres.render.com:5432/desarrollo_wb_db?sslmode=require";
+    private static final String USER = "desarrollo_wb_db_user";
+    private static final String PASSWORD = "IvpcqhH4uaApBCBmkep7yqcnrpIyHbJv";
+
     public static Connection getConexion() {
         Connection con = null;
         try {
